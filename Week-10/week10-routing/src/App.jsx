6d964fc3ec1696b90881a09e27eef6cd9d17d504
sdt03 <dateraoshoumik@gmail.com>
2024-12-15 10:38:58 +0530

    Week-10 Commit

---
+++ Week-10/week10-routing/src/App.jsx
@@ -0,0 +1,7 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+
+}
+
+export default App
---
+++ week-10.2/state-experiments/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return <div>
+    <LightBulb />
+  </div>
+}
+
+function LightBulb(){
+  const [bulbOn, setBulbOn] = useState(true);
+  return <div>
+    <BulbState bulbOn={bulbOn}/>
+    <ToggleBulbState bulbOn={bulbOn} setBulbOn={setBulbOn}/>
+  </div>
+}
+
+function BulbState({bulbOn}){
+  return <div>
+    {bulbOn ? "Bulb on":"Bulb off"}
+  </div>
+} 
+
+function ToggleBulbState({bulbOn, setBulbOn}){
+  function toggle(){
+    setBulbOn(currentState => !currentState)
+  }
+  
+  return <div>
+    <button onClick={toggle}>Toggle Button</button>
+  </div>
+}
+
+export default App
