0b4b214cec0c3562fe44f0029c837dca9aa28c7d
sdt03 <dateraoshoumik@gmail.com>
2024-12-19 02:07:57 +0530

    Created Recoil

---
+++ Week-11/Week-11.2/Week-11.2-Recoil/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+import { RecoilRoot, atom, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { counterAtom } from './store/atoms/counter'
+
+function App() {
+  return (
+    <RecoilRoot>
+      <Counter />
+    </RecoilRoot>
+  )
+}
+
+function Counter() {
+  return <div>
+    <CurrentCount />
+    <Increase />
+    <Decrease />
+  </div>
+}
+
+function CurrentCount(){
+  const count = useRecoilValue(counterAtom);
+  return <div>
+    {count}
+  </div>
+}
+
+function Increase(){
+  const setCount = useSetRecoilState(counterAtom);
+
+  function increase(){
+    setCount(c => c+1);
+  }
+
+  return <div>
+    <button onClick={increase}>Increase</button>
+  </div>
+}
+
+function Decrease() {
+  const setCount = useSetRecoilState(counterAtom);
+
+  function decrease(){
+    setCount(c => c-1);
+  }
+
+  return <div>
+    <button onClick={decrease}>Decrease</button>
+  </div>
+}
+
+export default App
