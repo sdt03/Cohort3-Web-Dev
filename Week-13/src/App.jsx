90deffc607f84553da4b18a72039fa6f3bb74e51
sdt03 <dateraoshoumik@gmail.com>
2025-02-09 13:27:26 +0530

    Update App.jsx

--- Week-13/src/App.jsx
+++ Week-13/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
 import { Button } from './components/Buttons';
-import { Otp } from './components/Otp';
+import OTPLogin from './components/Otp';
 
 function App(){
   return(
     <>
      <div className='h-screen bg-blue-800'>
-      <Otp number={6}/>
+      <OTPLogin/>
      </div>
     </>
   )
