3903a09409cc39dbfe097135b16eac504ca2e3d2
sdt03 <dateraoshoumik@gmail.com>
2025-02-22 23:06:45 +0530

    share endpoint

--- Week15-Second_Brain/src/index.ts
+++ Week15-Second_Brain/src/index.ts
@@ -3,9 +3,10 @@ import {z} from 'zod';
 import { JWT_SECRET } from './config';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import { ContentModel, UserModel } from './database/schema';
+import { ContentModel, UserModel, LinkModel } from './database/schema';
 import connectDB from './database/db.service';
 import { userMiddleware } from './middleware/useAuth';
+import { random } from './utils';
 
 const app = express();
 app.use(express.json());
@@ -132,6 +133,74 @@ app.delete("/api/v1/content", userMiddleware, async (req, res)=>{
         "message": "Deleted"
     });
 });
+
+app.post("/api/v1/share", userMiddleware, async(req, res)=>{
+    const share = req.body.share;
+
+    if(share){
+        const existingLink = await LinkModel.findOne({
+            userId: req.userId
+        });
+        if(existingLink){
+            res.json({
+                hash: existingLink.hash
+            });
+            return
+        }
+        const hash = random(10);
+        await LinkModel.create({
+            userId: req.userId,
+            hash: hash
+        });
+
+        res.json({
+            hash
+        });
+    } else {
+        await LinkModel.deleteOne({
+            userId: req.userId,
+        });
+        res.json({
+            message: "Remove Link"
+        });
+    }
+})
+
+app.get("/api/v1/:shareLink", async(req, res)=>{
+    const hash = req.params.shareLink;
+
+    const link = await LinkModel.findOne({
+        hash
+    });
+
+    if(!link){
+        res.status(401).json({
+            message: "Sorry incorrect input"
+        });
+        return;
+    }
+
+    const content = await ContentModel.find({
+        userId: link.userId
+    })
+    console.log(link);
+    const user = await UserModel.findOne({
+        _id: link.userId
+    });
+
+    if(!user){
+        res.status(411).json({
+            message: "user not found"
+        })
+        return;
+    }
+
+    res.json({
+        username: user.username,
+        content: content
+    })
+})
+
 
 app.listen(3000);
 console.log("Listening on port 3000");
