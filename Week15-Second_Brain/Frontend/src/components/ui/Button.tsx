f23d94d4279e350b60b921097bd440751d71230e
sdt03 <dateraoshoumik@gmail.com>
2025-02-26 22:56:23 +0530

    Update Button.tsx

--- Week15-Second_Brain/Frontend/src/components/ui/Button.tsx
+++ Week15-Second_Brain/Frontend/src/components/ui/Button.tsx
@@ -6,7 +6,7 @@ export interface ButtonProps{
     text: string;
     startIcon?: ReactElement;
     endIcon?: ReactElement;
-    onClick: () => void;
+    onClick?: () => void;
 }
 
 const defaultStyles = "rounded-md px-4 py-2 flex font-light items-center"
@@ -22,6 +22,11 @@ const variantStyles = {
     "secondary": "bg-purple-200 text-purple-600"
 }
 
-export const Button = (props: ButtonProps) => {
-    return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}>{props.startIcon? <div className="pr-2">{props.startIcon}</div> : null} {props.text}</button>
+export const Button = ({variant, size, text, startIcon, endIcon, onClick}: ButtonProps) => {
+    return <button onClick={onClick} className={`${variantStyles[variant]} ${defaultStyles} ${sizeStyles[size]} cursor-pointer`}>
+        {startIcon? <div className="pr-2">
+            {startIcon}
+        </div> : null} 
+    {text}
+    </button>
 }   
