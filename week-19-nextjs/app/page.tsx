eb07a2715f1796fe845c458d646c5b45dc6f2dcb
sdt03 <dateraoshoumik@gmail.com>
2025-03-27 00:18:58 +0530

    Init

---
+++ week-19-nextjs/app/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Home() {
+
+  return (
+   <div className="text-lg w-screen h-screen flex items-center justify-center">
+    <div>
+      Todo application
+
+      <br />
+      <Link className="text-md border m-2" href="/signin">Sign in to Todo app</Link>
+      <br />
+      <Link className="text-md border m-2" href="/signup">Sign up to Todo app</Link> 
+    </div>
+   </div>
+  );
+}
