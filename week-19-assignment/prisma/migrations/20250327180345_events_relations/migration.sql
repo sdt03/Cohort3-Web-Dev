6224441909736bb6617fa98d73863a2e8d56efa9
sdt03 <dateraoshoumik@gmail.com>
2025-03-27 23:34:00 +0530

    prisma schema

---
+++ week-19-assignment/prisma/migrations/20250327180345_events_relations/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "location" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdById" INTEGER NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
